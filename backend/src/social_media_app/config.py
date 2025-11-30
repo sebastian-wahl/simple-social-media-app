163917faa75d5ed4cc38d3d6691bb555193e90f1
Sebastian Wahl <sebastian.wahl@evolit.com>
2025-11-30 19:33:15 +0100

    Refactor .env.template into root, adapt config.py in backend

--- backend/src/social_media_app/config.py
+++ backend/src/social_media_app/config.py
@@ -10,6 +10,7 @@ class Settings:
     APP_ENV: str = os.getenv("APP_ENV", "dev")
     APP_HOST: str = os.getenv("APP_HOST", "0.0.0.0")
     APP_PORT: int = int(os.getenv("APP_PORT", "8000"))
+
     DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///app.db")
 
     MINIO_ENABLED = os.getenv("MINIO_ENABLED", "true").lower() == "true"
